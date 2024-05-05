8e9f9c67cb8ca8a7c28fa60c537f93c595ef32a5
Rodrigo Gargani Oliveira <mjgargani@gmail.com>
2024-05-05 19:39:44 -0300

    feat: add redis cache store

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MoviesModule } from './movies/movies.module';
 import { DatabaseModule } from './database/database.module';
-import { ConfigModule } from '@nestjs/config';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as redisStore from 'cache-manager-redis-store';
+import { RedisClientOptions } from 'redis';
 @Module({
   imports: [
-    CacheModule.register({
+    CacheModule.registerAsync<RedisClientOptions>({
+      imports: [ConfigModule],
+      inject: [ConfigService],
       isGlobal: true,
+      useFactory: async (configService: ConfigService) => ({
+        store: redisStore,
+        url: configService.get<string>('REDIS_URL'),
+      }),
     }),
     ConfigModule.forRoot({
       isGlobal: true,
